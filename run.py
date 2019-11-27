62ef53297efe9c5569ed7c99f38fb0551cdc171d
lovinervy <35933009+lovinervy@users.noreply.github.com>
2019-11-27 20:58:04 +0500

    Add files via upload

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gatariapi as ga
 
-user_id = 1015
+user_id = 2208
 def userinformation(user_id):
     r = ga.user_info(user_id)
     if (r["code"] == 200):
@@ -42,8 +42,44 @@ def recent_plays(user_id):
 
 def activity(user_id):
     r = ga.user_activity(user_id, 0)
+    data = r["data"]
+    for i in data:
+        title = i["log"]
+        l = title.find("'/")
+        r = title.find("'>")
+        text = title[:-8+l] + title[r+2: -4]
+        edit_text = text.replace('<b>', '').replace('</b>', '')
+        print("rank:(", i["data"][1]["rank"],') ', edit_text)
+        print('link: https://osu.gatari.pw/' + title[l+2: +r])
+
+song_id = 1842043
+def check_map(user_id, song_id):
+    r = ga.score_on_map(user_id, song_id)
+    score = r['score']
+    x = float('{:.2f}'.format(score["accuracy"]))
+    print("accuracy: {}%, rank: {}".format(x, score["rank"]))
+    print(str(score["count_300"]) + '/'+ str(score["count_100"]) + '/' + str(score["count_50"]) \
+        + '/' + str(score["count_miss"]))
+    print('combo:', score["max_combo"])
+    print('top:', score["top"])
+
+
+def pp_checker(user_id):
+    r = ga.pp_values(user_id)
     print(r)
+    data = r["data"]
+    if(data["-59"]== None):
+        print('За день: 0.0pp')
+        print('За неделю:', data["-58"] - data["-53"])
+        print('За неделю:', data["-58"] - data["-29"])
+    else:
+        result1 = data["-59"] - data["-58"]
+        print(result1)
+
+    
 
 
 #recent_plays(user_id)
-userinformation(user_id)
+activity(user_id)
+#check_map(user_id, song_id)
+#pp_checker(user_id)
