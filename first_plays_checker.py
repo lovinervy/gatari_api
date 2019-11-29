92e4371994673e24ca6ce5f66c4551be92791bcf
lovinervy <35933009+lovinervy@users.noreply.github.com>
2019-11-29 09:15:40 +0500

    Перписал полностью заново, теперь не так сильно стыдно

--- first_plays_checker.py
+++ first_plays_checker.py
@@ -1,36 +1,36 @@
 import requests
 import json
 import time
-#шит код, написано чтобы просто было написано
+
+
 def user_activity():
     URL = "https://api.gatari.pw/user/events?u=2208&mode=0"
     user_information = requests.get(URL).json()
     return user_information
+
+
 time_id = None
 lost_id = None
+
+
 def push():
     global lost_id, time_id
     r = user_activity()
-    data = r["data"]
-    title = data[0]["log"]
+    data = r['data']
+    title = data[0]['log']
+    log_id = data[0]['id']
+    log_check_id = data[0]['type']
     l = title.find("'/")
     r = title.find("'>")
-    ll = title.find("has")
-    rr = title.find("place")
-    text_lost = title[-1:rr] + title[ll:+20]
-    b_id = title[l+2: +r]
+    beatmap_id = title[l+2: +r]
     text = title[:-8+l] + title[r+2: -4]
-    edit_text = text.replace('<b>', '').replace('</b>', '')
-    if (b_id != lost_id) and (text_lost =='has lost first place') :
-        lost_id = b_id
-        time_id = data[0]['id']
-        print ('You',edit_text,'\n link: https://osu.gatari.pw/' + str(b_id))
-    elif(b_id == lost_id) and (time_id != data[0]['id']) and (text_lost =='has lost first place'):
-        time_id = data[0]['id']
-        print ('You',edit_text,'\n link: https://osu.gatari.pw/' + str(b_id))
+    if (log_id != lost_id) and (log_check_id == 0):
+        lost_id = log_id
+        print('You', text, f'\n link: https://osu.gatari.pw/{beatmap_id}')
 
-def run():
-    while True:
-        push()
-        time.sleep(60) 
-run()
+n = 0
+while True:
+    n +=1
+    push()
+    if(n==3):break
+    time.sleep(5)
